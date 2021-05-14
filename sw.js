5485bf1219b701519307da96e55e56bb2e10d08b
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2021-05-14 16:30:55 +0530

    Updates

--- sw.js
+++ sw.js
@@ -121,9 +121,10 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 self.addEventListener('notificationclick', function (event) {
   console.log('[Service Worker] Notification click received.');
   event.notification.close();
-  event.waitUntil(clients.openWindow('https://selfregistration.cowin.gov.in/'));
-  self.registration.unregister().then(function () {
-    console.log('unregistered');
-  });
+  event.waitUntil(clients.openWindow('https://selfregistration.cowin.gov.in/').then(function () {
+    self.registration.unregister().then(function () {
+      console.log('unregistered');
+    });
+  }));
 });
 },{}]},{},["NqYy"], null)
